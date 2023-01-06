84e26453acd3e6b35e3a3b0f106641d99c6cefa6
Lucas Ferreira da Silva <lucferreira14@gmail.com>
2023-01-06 16:17:51 -0300

    init commit

---
+++ src/main/java/com/lucferreira/myanimeback/MyAnimeBackApplication.java
@@ -0,0 +1,13 @@
+package com.lucferreira.myanimeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAnimeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAnimeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucferreira/myanimeback/MyAnimeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucferreira.myanimeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAnimeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
