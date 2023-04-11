01e0a8827648cf96dc062747bd6180745d7f31ef
Lucas Ferreira da Silva <lucferreira14@gmail.com>
2023-04-11 02:10:27 -0300

    add search endpoint

--- src/main/java/com/lucferreira/myanimeback/controller/SnapshotController.java
+++ src/main/java/com/lucferreira/myanimeback/controller/SnapshotController.java
@@ -1,6 +1,9 @@
 package com.lucferreira.myanimeback.controller;
 
 import com.lucferreira.myanimeback.exception.WaybackException;
+import com.lucferreira.myanimeback.exception.WaybackTimestampParseException;
+import com.lucferreira.myanimeback.service.ScrapeService;
+import com.lucferreira.myanimeback.service.WaybackService;
 import com.lucferreira.myanimeback.service.wayback.ResponseSnapshot;
 import com.lucferreira.myanimeback.service.wayback.WaybackMachineClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,14 +22,17 @@ import java.util.Optional;
 public class SnapshotController {
 
     @Autowired
-    private WaybackMachineClient waybackClient;
-    @GetMapping("")
-    public ResponseEntity<ResponseSnapshot> search(@RequestParam(value = "url", required = true) String url, Optional<String> first, Optional<String> timestamp) throws WaybackException {
-        if (first.isPresent() && Boolean.valueOf(first.get())) {
-            ResponseSnapshot responseSnapshot  = waybackClient.getFirstSnapshot(url);
-            return ResponseEntity.ok(responseSnapshot);
+    private WaybackService waybackService;
+    @Autowired
+    private ScrapeService scrapeService;
+    @GetMapping("/search")
+    public ResponseEntity<List<ResponseSnapshot>> searchByUrl(@RequestParam(value = "url", required = true) String url, Optional<String> timestamp) throws WaybackException {
+        if(timestamp.isPresent()){
+            String strTimestamp = timestamp.get();
+            List<ResponseSnapshot> responseSnapshots = waybackService.getSnapshotByTimestamp(url, strTimestamp);
+            return ResponseEntity.ok(responseSnapshots);
         }
-        ResponseSnapshot responseSnapshot  = timestamp.isPresent() ? waybackClient.getSnapshot(url,timestamp.get()) : waybackClient.getSnapshot(url);
-        return ResponseEntity.ok(responseSnapshot);
+        List<ResponseSnapshot> responseSnapshots   =  waybackService.getSnapshotList(url);
+        return ResponseEntity.ok(responseSnapshots);
     }
 }
